d9adeae4476c23bcf61fb80c1f4064fddb2ae857
guptaraghav1999 <guptaraghav1999@gmail.com>
2020-01-11 23:17:47 +0530

    Authorize

--- ocpp_client.py
+++ ocpp_client.py
@@ -11,25 +11,42 @@ class ChargePoint(cp):
         request = call.BootNotificationPayload(
                 charging_station={
                     'model': 'Wallbox XYZ',
-                    'vendor_name': 'anewone'
+                    'vendorName': 'anewone'
                 },
                 reason="PowerUp"
         )
         response = await self.call(request)
-
+        
         if response.status == 'Accepted':
             print("Connected to central system.")
+            print(response)
 
+
+    async def send_authorize(self):
+        request = call.AuthorizePayload(
+                idToken={
+                    'idToken': '1234567890',
+                    'type': 'Central'
+                },
+                # _15118_certificate_hash_data=None,
+                # evse_id=None
+        )
+        response = await self.call(request)
+        if response.id_token_info['status'] == 'Accepted':
+            print("Authorization Sucessful")
+            print(response)
 
 async def main():
     async with websockets.connect(
-        'ws://192.168.43.29:9000/CP_1',
+        'ws://192.168.43.217:9000/CP_1',
          subprotocols=['ocpp2.0']
     ) as ws:
 
         cp = ChargePoint('CP_1', ws)
 
         await asyncio.gather(cp.start(), cp.send_boot_notification())
+        await asyncio.gather(cp.start(), cp.send_authorize())
+        
 
 
 if __name__ == '__main__':
