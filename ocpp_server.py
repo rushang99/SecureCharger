464f0c94a22f94f5a93a42d4a7b1341898074bdd
rushang99 <rushang.gupta99@gmail.com>
2020-01-12 00:22:02 +0530

    variables set

--- ocpp_server.py
+++ ocpp_server.py
@@ -9,6 +9,7 @@ from ocpp.v20 import call_result
 class ChargePoint(cp):
     @on('BootNotification')
     def on_boot_notitication(self, charging_station, reason, **kwargs):
+        print('BootNotification')
         return call_result.BootNotificationPayload(
             current_time = datetime.utcnow().isoformat(),
             interval = 10,
@@ -17,6 +18,7 @@ class ChargePoint(cp):
 
     @on('Authorize')
     def on_authorize(self, id_token, **kwargs):
+        print('Authorize')
         # can add certificate _15118_certificate_hash_data
         return call_result.AuthorizePayload(
             id_token_info = {
@@ -27,6 +29,24 @@ class ChargePoint(cp):
             }
             # certificate_status =
             # evse_id =
+        )
+
+    @on('CancelReservation')
+    def on_cancel_reservation(self, reservation_id):
+        print('CancelReservation')
+        return call_result.CancelReservationPayload(
+            status = 'Accepted'
+        )
+
+    @on('SetVariables')
+    def set_variables(self, set_variable_data):
+        print('SetVariables')
+        return call_result.SetVariablesPayload(
+            set_variable_result = [{
+                'attributeStatus':'Accepted',
+                'component' : {'name': 'ComponentType'},
+                'variable': {'name': 'VariableType'}
+            }]
         )
 
     
